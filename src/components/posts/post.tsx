a1f5d7e138a7088b6377cec657ee6878fee51d17
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-04-28 17:06:14 -0700

    Update Post Component

---
+++ src/components/posts/post.tsx
@@ -0,0 +1,14 @@
+import './post.css';
+export const Post = () =>{
+    return (
+        <section className='post'>
+            <p>Back to HomePage</p>
+            <h3>Posts of Martin</h3>
+           <article className='post__info'>
+              <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, a!</h4>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus esse amet obcaecati rerum distinctio nisi facilis voluptate aut voluptatibus quasi!</p>
+
+           </article>
+        </section>
+    );
+}
