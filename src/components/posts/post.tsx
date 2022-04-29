ff57a626c15ead29dee9c4030b331e8145cd54b8
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-04-29 09:01:30 -0700

    Update post.tsx

--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -1,12 +1,16 @@
 import './post.css';
-export const Post = () =>{
+
+interface Props{
+    title: string
+    body: string
+}
+
+export const Post = (props:Props) =>{
     return (
         <section className='post'>
-            <p>Back to HomePage</p>
-            <h3>Posts of Martin</h3>
            <article className='post__info'>
-              <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, a!</h4>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus esse amet obcaecati rerum distinctio nisi facilis voluptate aut voluptatibus quasi!</p>
+              <h4>{props.title}</h4>
+              <p>{props.body}</p>
 
            </article>
         </section>
