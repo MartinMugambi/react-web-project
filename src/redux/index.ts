a7e8790b3bc4823254676f6693d417e39e2234e4
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-04-29 09:41:51 -0700

    Update index.ts

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,5 +1,8 @@
+export { username } from "./user/userSlice";
+
 export { postId } from "./user/userSlice";
 
 export { fetchPost } from "./post/postSlice";
  
 export { fetchUserData } from "./user/userSlice";
+
