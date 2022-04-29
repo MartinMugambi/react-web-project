c7faf7bbe5d3616b84abe0dad433c9a77903a17c
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-04-29 09:01:16 -0700

    Create post.tsx

---
+++ src/components/postList/post.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import {useSelector, useDispatch} from 'react-redux'
+import { fetchPost } from "../../redux";
+import { RootState } from "../../redux/store";
+import { Post } from "../posts/post";
+import  './post.css';
+interface PostData{
+    userId: number
+    id:number
+    title: string
+    body: string
+}
+
+const PostList = () =>{
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+      dispatch(fetchPost())
+    },[dispatch])
+
+    const postData: PostData[] = useSelector((state: RootState) => state.post.data);
+    const loading = useSelector((state:RootState) => state.post.loading);
+    const id = useSelector((state:RootState) => state.user.id);
+
+    const filteredPost = postData.filter(post => post.userId === id)
+
+    console.log(filteredPost);
+    
+    console.log("id is", id);
+    
+
+    return (
+        <section className="">
+            <p>Back to HomePage</p>
+            <h3>Posts of Martin</h3>
+        <article className="details">
+            {filteredPost.map(post => <Post  key={post.id}  title = {post.title} body ={post.body}/>)}
+        </article>
+        </section>
+    );
+}
+
+export default PostList;
