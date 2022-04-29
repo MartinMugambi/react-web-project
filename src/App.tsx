72ce011ed417fe6ddddc4ad23879d71ff134b471
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-04-28 19:32:57 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Post } from './components/posts/post';
+import { UserList } from './components/userList/userList';
 import { User } from './components/users/user';
 
 function App() {
   return (
    <>
-   <User />
+   <UserList />
    {/* <Post /> */}
    </>
   );
