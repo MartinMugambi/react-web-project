be5cffb83a351436ae138bbaa6814482a4d265bb
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-05-01 14:01:03 -0700

    Update post.tsx

--- src/components/postList/post.tsx
+++ src/components/postList/post.tsx
@@ -5,12 +5,8 @@ import { RootState } from "../../redux/store";
 import { Post } from "../posts/post";
 import  './post.css';
 import {TailSpin} from "react-loader-spinner"
-interface PostData{
-    userId: number
-    id:number
-    title: string
-    body: string
-}
+import  {useNavigate} from "react-router-dom"
+import {PostData} from "../../types/types"
 
 const PostList = () =>{
     const dispatch = useDispatch();
@@ -18,6 +14,8 @@ const PostList = () =>{
     useEffect(()=>{
       dispatch(fetchPost())
     },[dispatch])
+
+   const navigate = useNavigate();
 
     const postData: PostData[] = useSelector((state: RootState) => state.post.data);
     const loading = useSelector((state:RootState) => state.post.loading);
@@ -28,9 +26,16 @@ const PostList = () =>{
     const filteredPost = postData.filter(post => post.userId === id)
 
     
+
+    // go to previous page
+
+     const previousPage = () =>{
+         navigate(-1)
+     }
+
     return (
         <section className="">
-            <p>Back to HomePage</p>
+            <p onClick={previousPage}>Back to HomePage</p>
             <h3>Posts of {username}</h3>
         <article className="details">
             {loading && <TailSpin height={100} width = {100} color ="purple" ariaLabel="loading" />}
