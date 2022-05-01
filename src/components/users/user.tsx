5b8103c256a59c97baf80b56ebfb6afc9ec6539d
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-05-01 11:45:40 -0700

    Update user.tsx

--- src/components/users/user.tsx
+++ src/components/users/user.tsx
@@ -3,6 +3,8 @@ import {PencilAltIcon} from '@heroicons/react/solid'
 import {useDispatch} from "react-redux"
 import { postId, username } from '../../redux';
  import {useNavigate} from "react-router-dom"
+import React, { ChangeEvent, useState} from 'react';
+
 
 interface  Props{
     name: string
@@ -15,23 +17,51 @@ interface  Props{
 
 export const User = (props: Props) =>{
   
+ const[edit, setEdit] = useState<boolean>(true)
+ 
+ const[name, setName] = useState<string>(props.name);
+
+
+
 const dispatch = useDispatch();
 const navigate = useNavigate()
 
   const handleClick =() =>{
       props.setId(props.id);
       dispatch(postId(props.id));
-      dispatch(username(props.name))
+      dispatch(username(name))
       navigate('post')
   }
 
-  console.log(props.postId);
   
+  //toggleEditButton
+
+  const makeEditable=() =>{
+    setEdit(prev => !prev)
+  }
+  
+  //change name inputfield
+
+   const changeNameInput =(event: ChangeEvent<HTMLInputElement>) =>{
+     setName(event.target.value);
+    
+   }
+
+   //submitInput field to redux store
+
+   const submitInputField = (event: React.KeyboardEvent) =>{
+       if(event.key === "Enter"){
+        dispatch(username(name));
+        setEdit(true)
+       }
+   
+   }
+
     return (
          <section className='card'>
            <article className='card__details'>
-               <h1>{props.name}</h1>
-               <PencilAltIcon  className='card__icon' data-testid="icon"/>
+              <input type="text" value={name} disabled={edit} className ={ edit ? "card__input--false": "card__input"} onChange ={changeNameInput}  onKeyDown ={submitInputField} />
+               <PencilAltIcon  className='card__icon' data-testid="icon"  onClick={makeEditable}  />
            </article>
             <p className='card__title' data-testid ="username">@{props.username}</p>
            <article className='card__button' >
