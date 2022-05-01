c2ebd2c0540011ee17b4d13fa89ea87f08cde4d0
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-05-01 11:44:31 -0700

    Update post.tsx

--- src/components/postList/post.tsx
+++ src/components/postList/post.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {useSelector, useDispatch} from 'react-redux'
 import { fetchPost } from "../../redux";
 import { RootState } from "../../redux/store";
 import { Post } from "../posts/post";
 import  './post.css';
+import {TailSpin} from "react-loader-spinner"
 interface PostData{
     userId: number
     id:number
@@ -13,7 +14,7 @@ interface PostData{
 
 const PostList = () =>{
     const dispatch = useDispatch();
-
+  
     useEffect(()=>{
       dispatch(fetchPost())
     },[dispatch])
@@ -23,6 +24,7 @@ const PostList = () =>{
     const id = useSelector((state:RootState) => state.user.id);
     const username  = useSelector((state:RootState) => state.user.name);
 
+      
     const filteredPost = postData.filter(post => post.userId === id)
 
     
@@ -31,6 +33,7 @@ const PostList = () =>{
             <p>Back to HomePage</p>
             <h3>Posts of {username}</h3>
         <article className="details">
+            {loading && <TailSpin height={100} width = {100} color ="purple" ariaLabel="loading" />}
             {filteredPost.map(post => <Post  key={post.id}  title = {post.title} body ={post.body}/>)}
         </article>
         </section>
