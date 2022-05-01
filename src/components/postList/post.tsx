04654af72f19303acd42185df4a19f7468edb9ab
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2022-05-01 14:29:00 -0700

    Update post.tsx

--- src/components/postList/post.tsx
+++ src/components/postList/post.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import {useSelector, useDispatch} from 'react-redux'
 import { fetchPost } from "../../redux";
 import { RootState } from "../../redux/store";
